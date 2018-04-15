dca2bb274569d557cd41850a120a9470489e7505
Carlos Rojas Morales <carlosrojo3000@yahoo.com.mx>
2018-04-15 18:55:45 -0500

    Función principal update 1.2

--- Examen1.c
+++ Examen1.c
@@ -12,30 +12,29 @@ int main(int argc, char const *argv[])
 	int n;
 	int ppid;
 	int children;
-	int *pipeptr;//Puntero a la tuberia
+	int pipeptr[2];//Puntero a la tuberia
 
 
 
 	printf("Indtrodusca el número de procesos hijo a crear: ");
 	scanf("%d", &children);
-	pipeptr= calloc(children+1,sizeof(ppid));
-	pipe(pipeptr);//Se declara la tuberia	
+	pipe(&pipeptr[0]);//Se declara la tuberia	
 
 	pid=fork();//Se crea un proceso hijo
 
 	if(pid>0){//Este es el proceso padre
-		close(*(pipeptr+1));
+		close(pipeptr[1]);
 		ppid=getpid();
-		n=write(*pipeptr,&ppid,sizeof(ppid));//Escribe el id del niño en la tuberia
+		n=write(pipeptr[0],&ppid,sizeof(ppid));//Escribe el id del niño en la tuberia
 		printf("Parent: Hello from parent!!, my id is: %d\n", ppid);	
 	} else if (pid==0)
 	{
 		int message;
-		//close (*pipeptr);
+		close (pipeptr[0]);
 		sleep(2);
 		ppid=getpid();
 		printf("Child: Hello from child!!, my id is: %d\n",ppid);
-		n=read(*pipeptr, &message,sizeof(message));
+		n=read(pipeptr[0], &message,sizeof(message));
 		printf("Child: Parent pid: %d\n", message);
 	}else{
 		printf("ERROR at fork\n");
