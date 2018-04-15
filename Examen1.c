3f9b8aa5e18f805c6111e46dd555c3b073bf1c9e
Carlos Rojas Morales <carlosrojo3000@yahoo.com.mx>
2018-04-14 20:09:28 -0500

    Función principal update 1

--- Examen1.c
+++ Examen1.c
@@ -13,12 +13,24 @@ int main(int argc, char const *argv[])
 		int n;
 		int message;
 		close(pp[1]);
-		n=write(pp[1],&pid,sizeof(pid));
+		n=write(pp[0],&pid,sizeof(pid));//Escribe el id del niño en la tuberia
 		ppid=getpid();
 		printf("Parent: Hello from parent!!, my id is: %d\n", ppid);
 		printf("Parent: Reading message from the pipe... \n");
 		while (wait(NULL)!=pid);
-		
+		n=read(pp[0], &message,sizeof(message));
+		printf("Parent: The message is: %d\n", message );		
+	} else if (pid==0)
+	{
+		int n;
+		int message = 65;
+		close(pp[0]);
+		ppid=getpid();
+		printf("Child: Hello from child!!, my id is: %d\n",ppid);
+		printf("Child: Sending message to the pipe\n");
+		n=write(pp[1], $message, sizeof(message));
+	}else{
+		printf("ERROR at fork\n");
 	}
 
 	return 0;
