c574be3f97323e0b9eebc9350ce7b64c44ea7f22
Carlos Rojas Morales <carlosrojo3000@yahoo.com.mx>
2018-04-15 19:56:28 -0500

    Función principal update 1.2

--- Examen1.c
+++ Examen1.c
@@ -20,11 +20,7 @@ int main(int argc, char const *argv[])
 	printf("Indtrodusca el numero de procesos hijo a crear: ");
 	scanf("%d", &children);
 	//pipe(&pipeptr[0]);//Se declara la tuberia	
-	for (int i = 0; i < children; ++i)
-	{
-		pid=fork();//Se crea un proceso hijo
-	}
-		
+	pid=fork();
 
 	if(pid>0){//Este es el proceso padre
 		//int n;
@@ -34,14 +30,21 @@ int main(int argc, char const *argv[])
 		printf("Parent: Hello from parent!!, my id is: %d\n", ppid);	
 	} else if (pid==0)
 	{
-		int delay=(rand()%3)+1;
+		while(children!=0){
+			if (pid==0)
+			{
+				children-=1;
+				pid=fork;
+			}
+		}
+		int delay=(rand()%30)+1;
 		//int n;
 		//int message;
 		//close (pipeptr[1]);
 		sleep(delay);
 		ppid=getpid();
 		printf("Child: Hello from child!!, my id is: %d\n",ppid);
-		printf("Child: Y have been slept for %d seconds\n",delay );
+		printf("Child: I have been slept for %d seconds\n",delay );
 		//n=read(pipeptr[0], &message,sizeof(message));
 		//printf("Child: Parent pid: %d\n", message);
 	}else{
