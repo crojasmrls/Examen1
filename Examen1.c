cb3b716898285d40da9d90609dd53782a14913f0
Carlos Rojas Morales <carlosrojo3000@yahoo.com.mx>
2018-04-15 19:43:09 -0500

    Función principal update 1.2

--- Examen1.c
+++ Examen1.c
@@ -11,32 +11,37 @@ int main(int argc, char const *argv[])
 	int cpid;
 	int ppid;
 	int children;
-	int pipeptr[2];//Puntero a la tuberia
+//	int pipeptr[2];//Puntero a la tuberia
 
 
 
-	printf("Indtrodusca el número de procesos hijo a crear: ");
+	printf("Indtrodusca el numero de procesos hijo a crear: ");
 	scanf("%d", &children);
-	pipe(&pipeptr[0]);//Se declara la tuberia	
-
-	pid=fork();//Se crea un proceso hijo
+	//pipe(&pipeptr[0]);//Se declara la tuberia	
+	for (int i = 0; i < children; ++i)
+	{
+		pid=fork();//Se crea un proceso hijo
+	}
+		
 
 	if(pid>0){//Este es el proceso padre
-		int n;
-		close(pipeptr[0]);
+		//int n;
+//		close(pipeptr[0]);
 		ppid=getpid();
-		n=write(pipeptr[1],&ppid,sizeof(ppid));//Escribe el id del niño en la tuberia
+//		n=write(pipeptr[1],&ppid,sizeof(ppid));//Escribe el id del hijoen la tuberia
 		printf("Parent: Hello from parent!!, my id is: %d\n", ppid);	
 	} else if (pid==0)
 	{
-		int n;
-		int message;
-		close (pipeptr[1]);
-		sleep(2);
+		int delay=(rand()%3)+1;
+		//int n;
+		//int message;
+		//close (pipeptr[1]);
+		sleep(delay);
 		ppid=getpid();
 		printf("Child: Hello from child!!, my id is: %d\n",ppid);
-		n=read(pipeptr[0], &message,sizeof(message));
-		printf("Child: Parent pid: %d\n", message);
+		printf("Child: Y have been slept for %d seconds\n",delay );
+		//n=read(pipeptr[0], &message,sizeof(message));
+		//printf("Child: Parent pid: %d\n", message);
 	}else{
 		printf("ERROR at fork\n");
 	}
