5fb3cd18123ac2a4012844469250091833e53eb9
Carlos Rojas Morales <carlosrojo3000@yahoo.com.mx>
2018-04-15 21:06:27 -0500

    Función principal update 1.2

--- Examen1.c
+++ Examen1.c
@@ -1,47 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
-#include <sys/wait.h>
-
 
 int main(int argc, char const *argv[])
 {
 	
 	int pid;//Variables para guardar id
-	int ppid;
-	int cpid;
-	int children;
-	int delay;
-	srand(time(NULL)); 
-//	int pipeptr[2];//Puntero a la tuberia
-
-
-
+	int ppid;//parent processes id
+	int cpid;// child processes id
+	int nchildren;//numero de procesos hijo
 	printf("Indtrodusca el numero de procesos hijo a crear: ");
-	scanf("%d", &children);
+	scanf("%d", &nchildren);
 	ppid=getpid();
 	printf("Parent: Hello from parent!!, my id is: %d\n", ppid);	
-	while(children!=0&&ppid==getpid()){
-		children=children-1;
+	while(nchildren!=0&&ppid==getpid()){
+		nchildren=nchildren-1;
 		pid=fork();
 		if (pid==0)
 		{
-
 			cpid=getpid();
 		}
 	}
 	if (cpid==getpid())
 	{
 		for (int i = 0; i < cpid; ++i)
-		{
 			rand();
-		}
-
-		delay=rand()%11;
+		int delay=rand()%11;
 		sleep(delay);
-		printf("Child: Hello from child!!, my id is: %d\n",cpid);
-		printf("Child: I have been slept for %d seconds\n",delay);
+		printf("Child: Hello from child!!, my id is: %d\nChild: I have been slept for %d seconds\n",cpid,delay);
 	}
 	return 0;
 }
